518eb61bdaf9f7a9388b051c689dda2fec11891c
Chordingg <whgustj5679@naver.com>
2024-04-08 16:13:02 +0900

    json, gson 변환

---
+++ ex01/src/main/java/org/zerock/domain/SampleDTO.java
@@ -0,0 +1,11 @@
+package org.zerock.domain;
+
+import lombok.Data;
+
+@Data	// setter, getter, toString, equals, hashCode 포함되어있음
+public class SampleDTO {
+
+	private String name;
+	private int age;
+	
+}
