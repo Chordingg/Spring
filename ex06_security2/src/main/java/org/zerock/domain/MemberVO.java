8d58d14a303464635292d3d0bafaaa9ae7d3a18f
Chordingg <whgustj5679@naver.com>
2024-05-07 19:19:30 +0900

    RequestMapping

--- ex06_security2/src/main/java/org/zerock/domain/MemberVO.java
+++ ex06_security2/src/main/java/org/zerock/domain/MemberVO.java
@@ -1,20 +1,22 @@
 package org.zerock.domain;
 
 import java.util.Date;
+
 import java.util.List;
 
 import lombok.Data;
-/*
- create table tbl_member(
-      userid varchar2(50) not null primary key,
-      userpw varchar2(100) not null,
-      username varchar2(100) not null,
-      regdate date default sysdate, 
-      updatedate date default sysdate,
-      enabled char(1) default '1'
-      );
 
+/*
+create table tbl_member(
+     userid varchar2(50) not null primary key,
+     userpw varchar2(100) not null,
+     username varchar2(100) not null,
+     regdate date default sysdate, 
+     updatedate date default sysdate,
+     enabled char(1) default '1'
+     );
 */
+
 
 @Data
 public class MemberVO {
